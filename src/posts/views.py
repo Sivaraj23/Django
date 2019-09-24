006e6bb12c79d508a7e79ded30689360fa919237
Sivaraj Pitchaikannu <sivarajp237@gmail.com>
2019-09-24 23:07:48 +0530

    24-09-2019's work

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-
+from .models import Post
 
 # Create your views here.
 
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
 
-def  posts_home(request):
-	return HttpResponse ("<h1>Hello</h1>")
+def post_detail(request, id=None): #retrieve
+	#instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
+def post_list(request): #list items
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset, 
+		"title":"Posts list"
+	}
+	return render(request, "index.html", context)
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
